ab3e59c6cd40a5d7adc46b6ff50489480fc1353d
ximing <ximing@meituan.com>
2017-11-16 00:26:56 +0800

    add core

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import React, {
     Component
 } from 'react';
 import ReactDOM from 'react-dom';
-import 'mxClient';
 import View from './view';
-console.log(mxClient)
+
 export default class DXMind {
     constructor(dom) {
         this.dom = dom;
@@ -22,34 +21,6 @@ export default class DXMind {
         }
         ReactDOM.render( < View / > , this.dom, () => {
             let container = document.getElementById('dxContainer');
-            if (!mxClient.isBrowserSupported()) {
-                // Displays an error message if the browser is not supported.
-                mxUtils.error('Browser is not supported!', 200, false);
-            } else {
-                // Disables the built-in context menu
-                mxEvent.disableContextMenu(container);
-
-                // Creates the graph inside the given container
-                var graph = new mxGraph(container);
-
-                // Enables rubberband selection
-                new mxRubberband(graph);
-
-                // Gets the default parent for inserting new cells. This
-                // is normally the first child of the root (ie. layer 0).
-                var parent = graph.getDefaultParent();
-
-                // Adds cells to the model in a single step
-                graph.getModel().beginUpdate();
-                try {
-                    var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
-                    var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-                    var e1 = graph.insertEdge(parent, null, '', v1, v2);
-                } finally {
-                    // Updates the display
-                    graph.getModel().endUpdate();
-                }
-            }
         })
     }
 }
